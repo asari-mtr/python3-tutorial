378d2708d7dc092ac1718567724fa21955e1b9db
Asari Mitsuteru <asari.mtr@gmail.com>
2018-09-09 23:36:03 +0900

    Add replace

---
+++ replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import re
+
+phone = "2004-959-559 # This is Phone Number"
+
+num = re.sub(r'#.*$', "", phone)
+print ("Phone num : ", num)
+
+num = re.sub(r'\D', "", phone)
+print ("Phone num : ", num)
