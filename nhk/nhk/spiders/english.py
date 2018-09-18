81f9ee5fa61e4738ed91942ee9fd5f81df4523e2
Asari Mitsuteru <asari.mtr@gmail.com>
2018-09-18 23:37:17 +0900

    Fix lint

--- nhk/nhk/spiders/english.py
+++ nhk/nhk/spiders/english.py
@@ -10,13 +10,13 @@ class EnglishSpider(scrapy.Spider):
     def parse(self, response):
         for data in response.xpath("//musicdata/music"):
             yield {
-                    'title': data.xpath("@title").extract_first(),
-                    'hdate': data.xpath("@hdate").extract_first(),
-                    'kouza': data.xpath("@kouza").extract_first(),
-                    'code': data.xpath("@code").extract_first(),
-                    'file': data.xpath("@file").extract_first(),
-                    'nendo': data.xpath("@nendo").extract_first(),
-                    'pgcode': data.xpath("@pgcode").extract_first()
+                'title': data.xpath("@title").extract_first(),
+                'hdate': data.xpath("@hdate").extract_first(),
+                'kouza': data.xpath("@kouza").extract_first(),
+                'code': data.xpath("@code").extract_first(),
+                'file': data.xpath("@file").extract_first(),
+                'nendo': data.xpath("@nendo").extract_first(),
+                'pgcode': data.xpath("@pgcode").extract_first()
             }
 
         for program in response.css("div.programListBox"):
@@ -24,7 +24,8 @@ class EnglishSpider(scrapy.Spider):
             if link is None:
                 continue
 
-            lesson = program.css('.title>a::attr(href)').extract_first().rsplit("/", 2)[-2]
+            lesson = program.css(
+                '.title>a::attr(href)').extract_first().rsplit("/", 2)[-2]
 
             if lesson is None:
                 continue
@@ -32,4 +33,3 @@ class EnglishSpider(scrapy.Spider):
             url = f"https://www2.nhk.or.jp/gogaku/st/xml/english/{lesson}/listdataflv.xml"
 
             yield scrapy.Request(url, callback=self.parse)
-
