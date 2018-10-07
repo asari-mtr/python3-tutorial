5fe2cf6e7f55ce3f995912add9eccf87c2aed3d0
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-08 00:30:48 +0900

    Init curses

--- teewt.py
+++ teewt.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python3
 
-import time
 import curses
 
-def pbar(window):
-    for i in range(10):
-        window.addstr(10, 10, "[" + ("=" * i) + ">" + (" " * (10 - i )) + "]")
-        window.refresh()
-        time.sleep(0.5)
+stdscr = curses.initscr()
 
-def main(stdscr):
-    stdscr.clear()
+curses.noecho()
 
-    for i in range(0, 9):
-        v = i - 10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+curses.cbreak()
 
-    stdscr.refresh()
-    key =  stdscr.getkey()
+stdscr.keypad(False)
+curses.echo()
 
-def win(stdscr):
-    s = "Hello world"
-    win = curses.newwin(5, 20, 5, 7)
-    win.box('|', '-')
-    win.addstr(1, 5, s)
-    win.refresh()
-    win.getch()
-
-
-curses.wrapper(win)
+curses.endwin()
