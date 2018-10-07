bf77ea6d88b411ff36d094510018a78707b277ae
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-08 00:37:10 +0900

    Use window

--- teewt.py
+++ teewt.py
@@ -6,11 +6,12 @@ from curses import wrapper
 def main(stdscr):
     stdscr.clear()
 
-    for i in range(0,11):
-        v = i - 10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+    begin_x = 20; begin_y = 7
+    height = 5; width = 40
+    win = curses.newwin(height, width, begin_y, begin_x)
 
     stdscr.refresh()
     stdscr.getkey()
+
 
 wrapper(main)
