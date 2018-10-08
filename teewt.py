ab0472dd8ff04550b5b92d7137e447d414918273
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-08 23:14:46 +0900

    Use pad

--- teewt.py
+++ teewt.py
@@ -5,13 +5,14 @@ from curses import wrapper
 
 def main(stdscr):
     stdscr.clear()
+    pad = curses.newpad(100, 100)
 
-    begin_x = 20; begin_y = 7
-    height = 5; width = 40
-    win = curses.newwin(height, width, begin_y, begin_x)
+    for y in range(0, 99):
+        for x in range(0, 99):
+            pad.addch(y, x , ord('a') + (x * x + y * y) % 26)
 
-    stdscr.refresh()
-    stdscr.getkey()
+    pad.refresh(5, 5, 10, 10, 20, 75)
+    pad.getkey()
 
 
 wrapper(main)
