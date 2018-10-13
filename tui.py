4644ebfbd7f59212a9d07442d42327b5259b66db
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-13 15:29:54 +0900

    Fix display

--- tui.py
+++ tui.py
@@ -16,8 +16,9 @@ post_list = [
         ]
 
 def refresh(pos=0):
-    for n in post_list:
-        print("\\e[1m%s\\e[m" % n)
+    for i, v in enumerate(post_list):
+        print("\33[3{}m\33[4{}m{}\33[m".format(i % 8, (i + 2) % 8, v))
+    print('\33[14F')
 
 def getchar():
     import sys, tty, termios
@@ -31,6 +32,8 @@ def getchar():
         termios.tcsetattr(fd, termios.TCSADRAIN, old)
     return ch
 
+clear = '\33[0J'
+print(clear)
 refresh()
 
 while True:
@@ -40,6 +43,6 @@ while True:
         print("quit")
         exit(0)
     else:
-        print("\e[0J")
+        print(clear)
         refresh()
 
