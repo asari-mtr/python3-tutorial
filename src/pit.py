1b0e75c6a48f126f667d55154c7472254b420838
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-22 00:18:48 +0900

    Add menu

--- src/pit.py
+++ src/pit.py
@@ -12,23 +12,35 @@ def update(stdscr):
 def make_panel(h, w, y, x, s):
     win = curses.newwin(h, w, y, x)
     win.clear()
-    win.border()
-    win.addstr(2, 2, s)
+    if h > 1:
+        win.border()
+        win.addstr(2, 2, s)
+    else:
+        win.addstr(0, 2, s)
 
     panel = curses.panel.new_panel(win)
     return win, panel
 
+def init_color():
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, 7, 23)
+
 def main(stdscr):
     "main"
+    init_color()
     curses.curs_set(0)
 
     update(stdscr)
 
     win1, panel1 = make_panel(5, 20, 15, 15, "Panel 1")
     win2, panel2 = make_panel(5, 20, 18, 18, "Panel 2")
-    tb = curses.textpad.Textbox(win1)
-    text = tb.edit() # terminate with ^G
-    stdscr.addstr(7, 0, text)
+    # tb = curses.textpad.Textbox(win1)
+    # text = tb.edit() # terminate with ^G
+    # stdscr.addstr(7, 0, text)
+
+    height, width = stdscr.getmaxyx()
+    sub = stdscr.subwin(height - 1, 0)
 
     curses.panel.update_panels()
 
@@ -44,8 +56,12 @@ def main(stdscr):
             stdscr.addstr(3, 2, "pressed {}({})".format(key, hex(key)))
         stdscr.addstr(4, 2, "panel1 {}".format(win1.getbegyx()))
         stdscr.addstr(5, 2, "panel2 {}".format(win2.getbegyx()))
+        sub.mvwin(height - 1, 0)
+        sub.bkgd(" ", curses.color_pair(1))
+        sub.addstr(0, 0, "Hello")
 
         stdscr.refresh()
+        sub.refresh()
         key = stdscr.getch()
         if key == 0x6a: # j
             y += 1
