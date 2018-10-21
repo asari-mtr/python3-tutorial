ebce26f25bb3b8f32d92bd30618a43586053bc50
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-21 12:24:01 +0900

    Set screen

--- src/pit.py
+++ src/pit.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 import curses
+from curses import textpad
+from curses import wrapper
 import time
 
+def update(stdscr):
+    stdscr.clear()
+    stdscr.border()
 
-stdscr = curses.initscr()
+def main(stdscr):
+    "main"
+    curses.curs_set(0)
 
-curses.noecho()
-curses.cbreak()
+    update(stdscr)
 
-stdscr.keypad(True)
+    key = ''
+    while True:
+        height, width = stdscr.getmaxyx()
+        stdscr.addstr(2, 2, "hello {} {}".format(height, width))
+        if not key == '':
+            stdscr.addstr(3, 2, "pressed {}".format(key))
+        stdscr.refresh()
+        key = stdscr.getch()
+        if key == 0x71: # q
+            break
+        if key == curses.KEY_RESIZE:
+            update(stdscr)
 
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-
-curses.endwin()
+wrapper(main)
