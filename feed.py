f9b92edfee70274ea05f67059145e103e5b2ccb0
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-23 09:30:05 +0900

    Get detail info by feed

--- feed.py
+++ feed.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import feedparser
 
-feed = feedparser.parse("https://qiita.com/tags/python/feed")
+args = sys.argv
 
-for entry in feed.entries:
-    print("%s [%s] (%s)" % (entry.title, entry.author, entry.url))
+if len(args) > 1:
+    tag = args[1]
+else:
+    tag = 'python'
+
+rss= feedparser.parse("https://qiita.com/tags/%s/feed" % tag)
+
+print("%s %s %s (%s)" % (rss.feed.title, rss.feed.link, rss.feed.description, rss.feed.updated))
+for entry in rss.entries:
+    print(" %s %s \33[1m%s\33[0m by %s (%s)" % (entry.id, entry.published, entry.title, entry.author, entry.url))
