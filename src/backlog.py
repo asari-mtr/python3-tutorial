2a8b1bab4cecd9666bb572ec43113ef1bad34a35
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-28 01:08:30 +0900

    Can use both tuple list and dict parameters

--- src/backlog.py
+++ src/backlog.py
@@ -34,9 +34,19 @@ class BacklogHandler:
         )
         return urlunparse(new_components)
 
-    def request(self, action, params=[]):
+    def request(self, action, params={}):
         url = "{}/api/v2/{}".format(self.endpoint, action)
-        queried_url = self.set_query(url, params)
+
+        tuple_list = []
+        if isinstance(params, dict):
+            for k, v in params.items():
+                tuple_list.append((k, v))
+        elif isinstance(params, list):
+            tuple_list = params
+        else:
+            raise ValueError('invalid params type')
+
+        queried_url = self.set_query(url, tuple_list)
 
         req = urllib.request.Request(queried_url, method = "GET")
         try:
@@ -84,6 +94,7 @@ class EsaHandler:
 
 handler = BacklogHandler()
 issues = handler.request('issues', [('projectId[]', 90134), ('statusId[]', 1), ('statusId[]', 2), ('count', 100)])
+issues = handler.request('issues', {'projectId[]': 90134, 'statusId[]': 1, 'count': 100})
 for issue in issues:
     print("[{}] ({}) {} {} by {}".format(issue['issueKey'], issue['status']['name'], issue['summary'], issue['dueDate'], issue['createdUser']['name']))
     comments = handler.request('issues/{}/comments'.format(issue['issueKey']), [('count', 1)])
