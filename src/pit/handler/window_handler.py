cf81978e729d6d05102d7e209a8c30b497f39602
Asari Mitsuteru <asari.mtr@gmail.com>
2018-10-29 10:44:43 +0900

    Add file

---
+++ src/pit/handler/window_handler.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../../')
+
+from pit.window.list_window import ListWindow
+class WindowHandler:
+    def __init__(self):
+        pass
---
+++ src/pit/window/list_window.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class ListWindow:
+    """
+    This class manage the list.
+
+    Gets a list and gives it behavior.
+    """
+    pass
