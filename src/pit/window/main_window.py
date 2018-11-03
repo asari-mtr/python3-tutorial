ab7df3fae9e095990e3627682f8ff20713ff51b6
Asari Mitsuteru <asari.mtr@gmail.com>
2018-11-03 23:28:10 +0900

    tweaks

--- src/pit/window/main_window.py
+++ src/pit/window/main_window.py
@@ -78,7 +78,6 @@ class MainWindow:
         # TODO: Calucurate 1
         display_height = height - 1
 
-        # TODO: Calucurate 48
         self.cursor = max(self.cursor - display_height, 0)
         self.offset = self.cursor
 
@@ -87,21 +86,21 @@ class MainWindow:
         # TODO: Calucurate 1
         display_height = height - 1
 
-        # TODO: Calucurate 48
-        self.cursor = min(self.cursor + display_height, 48)
-        if self.offset + display_height < 48:
+        last = len(self.model.list()) - 1
+        self.cursor = min(self.cursor + display_height, last)
+        if self.offset + display_height < last:
             self.offset = self.cursor
 
     def scroll(self, lines=1):
         height, width = self.stdscr.getmaxyx()
         # TODO: Calucurate 2
         display_height = height - 2
+        last = len(self.model.list()) - 1
         if lines > 0:
-            # TODO: Calucurate 48
-            self.cursor = min(self.cursor + lines, 48)
+            self.cursor = min(self.cursor + lines, last)
             absolute_y = self.cursor - self.offset
             if absolute_y > display_height:
-                self.offset = min(self.offset + lines, 48 - display_height)
+                self.offset = min(self.offset + lines, last - display_height)
         else:
             self.cursor = max(self.cursor + lines , 0)
             absolute_y = self.cursor - self.offset
