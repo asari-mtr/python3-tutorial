f1cc9e2b29f216fccd0e579df51ffc8d663787dd
Asari Mitsuteru <asari.mtr@gmail.com>
2018-11-15 23:02:57 +0900

    Move key handler

--- src/pit.py
+++ src/pit.py
@@ -6,53 +6,12 @@ from curses import wrapper
 from curses import panel
 import time
 
+from pit.handler.key_handler import get_request
 from pit.handler.window_handler import WindowHandler
 from pit.window.status_window import StatusWindow
 from pit.window.main_window import MainWindow
 
 from pit.request import Request
-from pit.key_map import KeyMap
-
-key_maps = {
-        KeyMap('Enter',     Request.ENTER),
-        KeyMap('Lt',        Request.BACK),
-        KeyMap('C-n',       Request.NEXT),
-        KeyMap('Down',      Request.NEXT),
-        KeyMap('J',         Request.NEXT),
-        KeyMap('C-p',       Request.PREVIOUS),
-        KeyMap('Up',        Request.PREVIOUS),
-        KeyMap('K',         Request.PREVIOUS),
-        KeyMap(',',         Request.PARENT),
-        KeyMap('Tab',       Request.VIEW_NEXT),
-        KeyMap('R',         Request.REFRESH),
-        KeyMap('O',         Request.MAXIMIZE),
-        KeyMap('q',         Request.VIEW_CLOSE),
-        KeyMap('Q',         Request.QUIT),
-        KeyMap('C-c',       Request.QUIT),
-
-        KeyMap('j',         Request.MOVE_DOWN),
-        KeyMap('k',         Request.MOVE_UP),
-        KeyMap('C-f',       Request.MOVE_PAGE_DOWN),
-        KeyMap('C-b',       Request.MOVE_PAGE_UP),
-        KeyMap('C-d',       Request.MOVE_HALF_PAGE_DOWN),
-        KeyMap('C-u',       Request.MOVE_HALF_PAGE_UP),
-        KeyMap('g',         Request.MOVE_FIRST_LINE),
-        KeyMap('G',         Request.MOVE_LAST_LINE),
-
-        KeyMap('m',         Request.VIEW_MAIN),
-        KeyMap('F1',        Request.VIEW_GITHUB),
-        KeyMap('F2',        Request.VIEW_TWITTER),
-        KeyMap('F3',        Request.VIEW_FEED),
-        KeyMap('F4',        Request.VIEW_BACKLOG),
-        KeyMap('F5',        Request.VIEW_ESA),
-}
-
-def get_request(key):
-    for key_map in key_maps:
-        if key_map.alias == key:
-            return key_map.request
-
-    return Request.NOBIND
 
 def main(stdscr):
     "main"
