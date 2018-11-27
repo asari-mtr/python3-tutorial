d795875846bed5e9f621e6a571d0caa098578393
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2018-11-27 16:46:16 +0900

    WIP

--- src/pit/window/body_window.py
+++ src/pit/window/body_window.py
@@ -8,6 +8,48 @@ from pit.items import Item
 import textwrap
 from pit.window.base_window import BaseWindow
 
+class ItemType(IntEnum):
+    NONE                = auto()
+    ID                  = auto()
+    TITLE               = auto()
+    CREATED             = auto()
+    UPDATED             = auto()
+    STATUS              = auto()
+    AUTHOR_ID           = auto()
+    AUTHOR_NAME         = auto()
+    CATEGORY            = auto()
+    LINK                = auto()
+    BODY                = auto()
+    LABELS              = auto()
+
+item_map = {
+    "id":                       ItemType.ID,
+    "title":                    ItemType.TITLE,
+    "created":                  ItemType.CREATED,
+    "updated":                  ItemType.UPDATED,
+    "status":                   ItemType.STATUS,
+    "author_id":                ItemType.AUTHOR_ID,
+    "author_name":              ItemType.AUTHOR_NAME,
+    "category":                 ItemType.CATEGORY,
+    "link":                     ItemType.LINK,
+    "body":                     ItemType.BODY,
+    "labels":                   ItemType.LABELS,
+}
+
+item_style_map = {
+    ItemType.ID                  : 3,
+    ItemType.TITLE               : 0,
+    ItemType.CREATED             : 2,
+    ItemType.UPDATED             : 2,
+    ItemType.STATUS              : 6,
+    ItemType.AUTHOR_ID           : 5,
+    ItemType.AUTHOR_NAME         : 5,
+    ItemType.CATEGORY            : 0,
+    ItemType.LINK                : 3,
+    ItemType.BODY                : 0,
+    ItemType.LABELS              : 3,
+}
+
 class BodyWindow(BaseWindow):
     def name(self):
         return "content"
@@ -100,3 +142,22 @@ class BodyWindow(BaseWindow):
 
         # TODO: Calucurate 2
         self.pad.refresh(self.offset, 0, 0, display_width, height - 2, width - 1)
+
+    def get_item(self, item,  key):
+        text = None
+        if key in ("created"):
+            text = self.date_format(item.created)
+        elif key in ("id"):
+            text = str(item.id)
+        elif key in ("status"):
+            text = item.status
+        elif key in ("author_name"):
+            text = item.author_name
+        elif key in ("title"):
+            text = item.title
+
+        attr = None
+        if key in item_map.keys():
+            attr = item_style_map[item_map[key]]
+
+        return text, attr
